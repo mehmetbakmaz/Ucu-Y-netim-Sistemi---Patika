0533fe54e4c7c56d11c61a1be68b63d05416a725
mehmetbakmaz <115632610+mehmetbakmaz@users.noreply.github.com>
2023-10-30 17:02:14 +0300

    Add files via upload

---
+++ Ucu Yonetim Sistemi - Patika/Ucu Yonetim Sistemi - Patika/Program.cs
@@ -0,0 +1,42 @@
+﻿public class HavaYolu
+{
+    public int Id { get; set; }
+    public string Isim { get; set; }
+    public List<Ucak> Ucaklar { get; set; }
+    public List<Pilot> Pilotlar { get; set; }
+    public List<Ucus> Ucuslar { get; set; }
+}
+
+public class Ucak
+{
+    public int Id { get; set; }
+    public string Tip { get; set; }
+    public bool CalisiyorMu { get; set; }
+    public int GerekliPilotSayisi { get; set; }
+}
+
+public class Ucus
+{
+    public int Id { get; set; }
+    public HavaAlani KalkisHavaAlani { get; set; }
+    public HavaAlani InisHavaAlani { get; set; }
+    public DateTime KalkisSaati { get; set; }
+    public DateTime InisSaati { get; set; }
+    public Pilot Pilot { get; set; }
+    public Pilot YardimciPilot { get; set; }
+    public Ucak Ucak { get; set; }
+}
+
+public class HavaAlani
+{
+    public int Id { get; set; }
+    public string Isim { get; set; }
+}
+
+public class Pilot
+{
+    public int Id { get; set; }
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DeneyimSeviyesi { get; set; }
+}
